320909ca3e3bc50a057efcdece8adcdc88b2365d
Alena Miloserdova <miloserdovaalena220@gmail.com>
2024-01-12 12:05:46 +0300

    исправление комментариев

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Button } from 'antd';
-import axios from 'axios';
-import type { ColumnsType } from 'antd/es/table';
-
-interface DataType {
-  country: string;
-  name: string;
-}
-
-const limit = 10;
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Страна',
-    dataIndex: 'country',
-    key: 'country',
-  },
-  {
-    title: 'Название школы',
-    dataIndex: 'name',
-    key: 'name',
-  },
-];
-
-const Root: React.FC = () => {
-  const [offset, setOffset] = useState<number>(0);
-  const [dataSource, setDataSource] = useState([]);
-
-  const getUniversity = async (offset: number, limit: number) => {
-    try {
-      const response = await axios.get(
-        `http://universities.hipolabs.com/search?offset=${offset*limit}&limit=${limit}`
-      );
-      console.log(response)
-      setDataSource(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  useEffect(() => {
-    getUniversity(offset, limit);
-  }, [offset]);
-
-  return (
-    <>
-      <Table<DataType> dataSource={dataSource} columns={columns} pagination={false} />
-      <div className="wrapper">
-          <p>Текущая страница: {offset+1}</p>
-        <div>
-          <Button className='btn' onClick={() => setOffset(offset - 1)} disabled={!offset}>
-            Назад
-          </Button>
-          <Button className='btn' onClick={() => setOffset(offset + 1)}>
-            Вперед
-          </Button>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Root;
+import React, { useEffect, useState } from 'react'; 
+import axios from 'axios'; 
+import { Table, Button } from 'antd'; 
+import type { ColumnsType } from 'antd/es/table'; 
+ 
+interface DataType { 
+  country: string; 
+  name: string; 
+  house: string; 
+} 
+ 
+const columns: ColumnsType<DataType> = [ 
+  { 
+    title: 'Страна', 
+    dataIndex: 'country', 
+    key: 'country', 
+  }, 
+  { 
+    title: 'Название школы', 
+    dataIndex: 'name', 
+    key: 'name', 
+  }, 
+  { 
+    title: 'Название города', 
+    dataIndex: 'house', 
+    key: 'house', 
+  }, 
+]; 
+ 
+const Sneakers = () => { 
+  const [page, setPage] = useState<number>(1); 
+  const [dataSource, setDataSource] = useState<DataType[]>([]); 
+ 
+  const getUniversity = async (page: number, limit: number) => { 
+    try { 
+      const response = await axios.get(`http://universities.hipolabs.com/search?offset=${(page - 1) * limit}&limit=${limit}`); 
+      setDataSource(response.data); 
+    } catch (error) { 
+      console.error('Error fetching data:', error); 
+    } 
+  };   
+ 
+  useEffect(() => { 
+    getUniversity(page, 5); 
+  }, [page]); 
+ 
+  return ( 
+    <> 
+      <Table dataSource={dataSource} columns={columns} pagination={false} /> 
+      <Button onClick={() => setPage(page - 1)} disabled={page === 1}> 
+        Назад 
+      </Button> 
+      <Button onClick={() => setPage(page + 1)}>Вперед</Button> 
+    </> 
+  ); 
+} 
+ 
+export default Sneakers;
