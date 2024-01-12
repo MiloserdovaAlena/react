106ebc6d4db0bbfdb47e97653c2605b4b4857047
Alena Miloserdova <miloserdovaalena220@gmail.com>
2024-01-12 12:15:33 +0300

    исправление комментариев

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,31 +1,56 @@
-import { ARTEM_ROUTE, ANNA_ROUTE, VALERA_ROUTE, TABLE_PAGINATION_ROUTE } from '../../app/routes/config';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
-export interface StyledProps {
-    height?: string;
-}
-
-const Navbar = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    const handleAuthClick = () => {
-        setIsAuthenticated(!isAuthenticated);
-    };
-
-    return ( 
-        <div>
-            <div className='route'>
-                <Link to={ARTEM_ROUTE} className='routeLink'>Артём</Link>
-                <Link to={ANNA_ROUTE} className='routeLink'>Аня</Link>
-                <Link to={VALERA_ROUTE} className='routeLink'>Валера</Link>
-                <Link to={TABLE_PAGINATION_ROUTE} className='routeLink'>Таблицы</Link>
-                <button className='btnAuto' onClick={handleAuthClick}>
-                    {isAuthenticated ? 'Войти' : 'Авторизация'}
-                </button>
-            </div>
-        </div>
-     );
-};
+import { ARTEM_ROUTE, ANNA_ROUTE, VALERA_ROUTE, TABLE_PAGINATION_ROUTE } from '../../app/routes/config'; 
+import { Link } from 'react-router-dom'; 
+import { createContext, useState } from 'react'; 
+ 
+ 
+interface AuthContextType { 
+  loginButtonText: string; 
+  toggleLogin: () => void; 
+} 
+ 
+const defaultValue: AuthContextType = { 
+  loginButtonText: '', 
+  toggleLogin: () => {}, 
+}; 
+ 
+const AuthContext = createContext<AuthContextType>(defaultValue); 
+ 
+const Navbar: React.FC = () => { 
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
+  const loginButtonText = isAuthenticated ? "Выйти" : "Войти"; 
+ 
+  const toggleLogin = () => { 
+    setIsAuthenticated((prevState) => !prevState); 
+  }; 
+  return( 
+    <div> 
+        <div> 
+          <Link to={ARTEM_ROUTE} className="routeLink"> 
+            О нас 
+          </Link> 
+          <Link to={ANNA_ROUTE} className="routeLink"> 
+            Главная 
+          </Link> 
+          <Link to={VALERA_ROUTE} className="routeLink"> 
+            Каталог 
+          </Link> 
+          {isAuthenticated && ( 
+            <Link to={TABLE_PAGINATION_ROUTE} className="routeLink"> 
+              Отзывы 
+            </Link> 
+          )} 
+        </div> 
+        <AuthContext.Provider value={{ loginButtonText, toggleLogin }}> 
+          <div> 
+            <button 
+              onClick={toggleLogin} 
+            > 
+              {loginButtonText} 
+            </button> 
+          </div> 
+        </AuthContext.Provider> 
+        </div> 
+  ); 
+}; 
  
 export default Navbar;
